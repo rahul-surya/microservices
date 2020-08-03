b26f229109bfb7114f1819b9009370a5552211fd
surya@gmail.com <surya@gmail.com>
2020-08-03 20:14:24 +0530

    first commit

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/surya/microservices/netflixzuulapigatewayserver/logging/ZuulLoggingFilter.java
@@ -0,0 +1,43 @@
+package com.surya.microservices.netflixzuulapigatewayserver.logging;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		
+		LOGGER.info("request: {} , uri: {}",request,request.getRequestURI());
+		
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
